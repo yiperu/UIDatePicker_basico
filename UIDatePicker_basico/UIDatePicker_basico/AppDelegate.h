98b279f4280ec2e2d8d3735c7376e93da284bc10
Henry AT <yiperu@gmail.com>
2013-09-30 08:44:15 -0500

    Inicializamos el proyecto

---
+++ UIDatePicker_basico/UIDatePicker_basico/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UIDatePicker_basico
+//
+//  Created by Henry AT on 9/30/13.
+//  Copyright (c) 2013 Henry AT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UIDatePicker_basico/UIDatePicker_basico/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  UIDatePicker_basico
+//
+//  Created by Henry AT on 9/30/13.
+//  Copyright (c) 2013 Henry AT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
